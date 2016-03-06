3ee55b0af0521bda8339a02376769605d3d6c129
asyed94 <aamir@syed.com>
2016-03-05 21:32:09 -0600

    Got new piece creation and collision working.

--- js/Game.js
+++ js/Game.js
@@ -1,151 +1,6 @@
 Tetris.Game = function(game) {
-	
-	this.init = function() {
-		console.log(this);
-		console.log(this.globo);
-		helper.call(this);
-	}
-
-	this.create = function() {
-
-	}
-
-	var helper = function() {
-		console.log(this);
-	}
-
-	// Define global variables here.
-	this.globo = 744834;
-
-	// Define global classes here.
-
-	/* Piece class: Creates and returns a new piece object from the given blockArrays.
-	 * Constructor takes the following parameters:
-	 * game - The phaser game instance.
-	 * gridWidth - Width of all 2D arrays in blockArrays.
-	 * gridHeight - Height of all 2D arrays in blockArrays.
-	 * blockArrays - An object that maps 4 2D arrays, which describe the piece 
-	 *                in all of its orientations, to their respective labels:
-	 *                UP, DOWN, LEFT, RIGHT.
-	 * gamePosX - The initial x position of the piece on the game board.
-	 * gamePosY - The initial y position of the piece on the game board.
-	 * initOrientation - The initial orientation of the piece ("UP", "DOWN", "LEFT", "RIGHT").
-	 * initGravity - The initial gravity given to all blocks in the piece.
-	 *
-	 * The arrays composing the blockArrays are interpreted as follows:
-	 * 0 : no block
-	 * 1 - 6 : block with color R, O, Y, G, B, V respectively
-     *	
-	 * ex: The following array describes a red T piece in the up orientation.
-	 * [ 0, 1, 0, 0,
-	 *   1, 1, 1, 0,
-	 *   0, 0, 0, 0,
-	 *   0, 0, 0, 0 ]
-	*/
-	this.Piece = function(game, gridWidth, gridHeight, blockArrays, gamePosX, gamePosY, initOrientation, initGravity) {
-		// Check and set the properties of a Piece
-		if (game != undefined) { this.game = game; }
-		else { return 1; }
-
-		if (gridWidth != undefined) { this.gridWidth = gridWidth; }
-		else { this.gridWidth = 1; }
-
-		if (gridHeight != undefined) { this.gridHeight = gridHeight; }
-		else { this.gridHeight = 1; }
-
-		if (this.blockArrays != undefined) { this.blockArrays = blockArrays; }
-		else {
-			this.blockArrays = {
-				UP: [0],
-				DOWN: [0],
-				LEFT: [0],
-				RIGHT: [0]
-			};
-		}
-
-		if (gamePosX != undefined) { this.gamePosX = gamePosX; }
-		else { this.gamePosX = 0; }
-
-		if (gamePosY != undefined) { this.gamePosY = gamePosY; }
-		else { this.gamePosY = 0; }
-
-		if (initOrientation != undefined) { this.currentOrientation = initOrientation; }
-		else { this.currentOrientation = "UP"; }
-
-		this.blocks = this.createPiece();
-	}
-
-	// Piece.createBlock: Creates a new block and returns it.
-	this.Piece.prototype.createBlock = function(gamePosX, gamePosY, color, initGravity) {
-		var newBlock = {
-			sprite: this.game.add.sprite(this.gamePosToCoord(gamePosX), this.gamePosToCoord(gamePosY), color),
-			gravity: initGravity,
-			gamePosX: gamePosX,
-			gamePosY: gamePosY,
-			removed: false
-		};
-		newBlock.sprite.scale = new PIXI.Point(1.5, 1.5);
-
-		return newBlock;
-	}
-
-	// Piece.createPiece: Creates the blocks for the pieces current orientation.
-	this.Piece.prototype.createPiece = function() {
-		// Get the blockArray for the currentOrientation
-		var currentBlockArray = this.blockArrays[this.currentOrientation];
-
-		// Define a holder for the new piece
-		var newPieceBlocks = [];
-
-		// For each element in the currentBlockArray...
-		var len = currentBlockArray.length;
-		for (i = 0; i < len; i ++) {
-			// Continue only if the element is greater than 0	
-			if (currentBlockArray[i] > 0) {
-
-				// Get this blocks relative position
-				var blockPosX = i % this.gridWidth;
-				var blockPosY = Math.floor(i / this.gridHeight);
-
-				// Make it a global postion
-				blockPosX += gamePosX;
-				blockPosY += gamePosY;
-
-				// Get the right color
-				var blockColor = undefined;
-				switch (blockArrays[i]) {
-					case 1:
-						blockColor = "red-block-img";
-						break;
-					case 2:
-						blockColor = "orange-block-img";
-						break;
-					case 3:
-						blockColor = "yellow-block-img";
-						break;
-					case 4:
-						blockColor = "green-block-img";
-						break;
-					case 5:
-						blockColor = "blue-block-img";
-						break;
-					case 6:
-						blockColor = "violet-block-img";
-						break;
-				}
-
-				// Create the block
-				newPieceBlocks.push(this.createBlock(blockPosX, blockPosY, blockColor, initGravity));
-			}
-
-			// Return the created array of blocks
-			return newPieceBlocks;
-		}
-	}
-
 };
 
-/*
 Tetris.Game.prototype = {
 
 	init: function() {
@@ -398,6 +253,48 @@ Tetris.Game.prototype = {
 		}
 	},
 
+	createPiece: function(blockArray, gamePosX, gamePosY, initGravity) {
+		var len, blockPosX, blockPosY, blockColor;
+		var newPiece = [];
+
+		len = blockArray.length;
+		for (i = 0; i < len; i++) {
+			if (blockArray[i] == 0) { continue; }
+			else {
+				// Get the blocks global position
+				blockPosX = gamePosX + (i % 4);
+				blockPosY = gamePosY + Math.floor(i/4);
+
+				// Resolve the blocks color
+				switch(blockArray[i]) {
+					case 1:
+						blockColor = "red-block-img";
+						break;
+					case 2:
+						blockColor = "orange-block-img";
+						break;
+					case 3:
+						blockColor = "yellow-block-img";
+						break;
+					case 4:
+						blockColor = "green-block-img";
+						break;
+					case 5:
+						blockColor = "blue-block-img";
+						break;
+					case 6:
+						blockColor = "violet-block-img";
+						break;
+				}
+			}
+
+			// Create the block and add it to newPiece 
+			newPiece.push(this.createBlock(blockPosX, blockPosY, blockColor, 1));
+		}
+
+		return newPiece
+	},
+
 	// Creates a new block at the given gamePos and adds it to this.blocks
 	// Returns the newly created block.
 	createBlock: function(gamePosX, gamePosY, color, initGravity) {
@@ -439,68 +336,3 @@ Tetris.Game.prototype = {
 	}
 
 };
-*/
-
-
-/* A quick note on scopes: it seems that Phaser redefines "this" to point to the game instance
- * instead of the function itself when it calls the init, preload, create, etc. functions.
- * The implications of this on the ease of my life are great :).
- */
-
-/*
-Tetris.Game.prototype.init = function() {
-	// Define global game variables here
-	console.log(this);
-	console.log(this.globo);
-
-	this.helper();
-}
-
-Tetris.Game.prototype.helper = function() {
-	console.log(this);
-}
-
-Tetris.Game.prototype.preload = function() {
-	// Load image assets
-	this.load.image("divider-img", "../assets/divider.png")
-	this.load.image("score-label-img", "../assets/score-label.png")
-	this.load.image("next-label-img", "../assets/next-label.png")
-	this.load.image("red-block-img", "../assets/red-block.png")
-	this.load.image("orange-block-img", "../assets/orange-block.png")
-	this.load.image("yellow-block-img", "../assets/yellow-block.png")
-	this.load.image("green-block-img", "../assets/green-block.png")
-	this.load.image("blue-block-img", "../assets/blue-block.png")
-	this.load.image("violet-block-img", "../assets/violet-block.png")
-}
-
-Tetris.Game.prototype.create = function() {
-}
-
-Tetris.Game.prototype.update = function() {
-	
-}
-Tetris.Game.prototype.executeStep = function() {
-	
-}
-Tetris.Game.prototype.updateBlocks = function() {
-	
-}
-Tetris.Game.prototype.clearCompletedRows = function() {
-	
-}
-Tetris.Game.prototype.applyGravity = function() {
-	
-}
-Tetris.Game.prototype.createRandStandardPiece = function() {
-	
-}
-Tetris.Game.prototype.createBlock = function() {
-	
-}
-Tetris.Game.prototype.gamePosToCoord = function() {
-	
-}
-Tetris.Game.prototype.coordToGamePos = function() {
-	
-}
-*/
