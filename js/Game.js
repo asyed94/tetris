8c93cef71f661e7db3546025e67725c37362c156
asyed94 <aamir@syed.com>
2016-03-12 19:14:59 -0600

    Added collision detection and removed old code.

--- js/Game.js
+++ js/Game.js
@@ -31,6 +31,10 @@ Tetris.Block = function(game, gamePosX, gamePosY, color, initGravitation) {
 };
 // Methods
 Tetris.Block.prototype = Object.create(Tetris.GameObject.prototype);
+// void: Moves this block up by 1 game unit.
+Tetris.Block.prototype.moveUp = function() {
+	this.gamePosY -= 1;
+}
 // void: Moves this block down by 1 game unit.
 Tetris.Block.prototype.moveDown = function() {
 	this.gamePosY += 1;
@@ -133,6 +137,13 @@ Tetris.Piece.prototype.remakeBlocks = function() {
 		}
 	}
 }
+// void: Moves all blocks in this piece up by 1 game unit.
+Tetris.Piece.prototype.moveUp = function() {
+	this.gamePosY -= 1;
+	for (block in this.blocks) {
+		this.blocks[block].moveUp();
+	}
+};
 // void: Moves all blocks in this piece down by 1 game unit.
 Tetris.Piece.prototype.moveDown = function() {
 	this.gamePosY += 1;
@@ -154,24 +165,60 @@ Tetris.Piece.prototype.moveLeft = function() {
 		this.blocks[block].moveLeft();
 	}
 };
-// void: Cycles the Piece's currentOrientation (UP, RIGHT, DOWN, LEFT).
-Tetris.Piece.prototype.rotate = function() {
+// void: Cycles the Piece's currentOrientation (UP, RIGHT, DOWN, LEFT) to the left.
+Tetris.Piece.prototype.rotateLeft = function() {
 	var orientationCycle = {
-		UP: "RIGHT",
-		RIGHT: "DOWN",
-		DOWN: "LEFT",
-		LEFT: "UP"
+		UP: "LEFT",
+		RIGHT: "UP",
+		DOWN: "RIGHT",
+		LEFT: "DOWN"
 	};
 
 	this.currentOrientation = orientationCycle[this.currentOrientation];
 
 	this.remakeBlocks();
 };
+// void: Cycles the Piece's currentOrientation (UP, RIGHT, DOWN, LEFT) to the right.
+Tetris.Piece.prototype.rotateRight = function() {
+	var orientationCycleRight = {
+		UP: "RIGHT",
+		RIGHT: "DOWN",
+		DOWN: "LEFT",
+		LEFT: "UP"
+	};
+
+	this.currentOrientation = orientationCycleRight[this.currentOrientation];
+
+	this.remakeBlocks();
+};
 // void: Apply this Piece's game properties to its block's sprites.
 Tetris.Piece.prototype.update = function() {
-	for (block in this.blocks) { this.blocks[block].update(); }
+	for (block in this.blocks) {
+		this.blocks[block].isGravitized = this.isGravitized;
+		this.blocks[block].update();
+	}
 };
-Tetris.Piece.prototype.settle = function() { // Block[]: Makes the Piece stationary, empties and returns its blocks.
+// void: Sets isGravitized for all Block objects in this Piece to given value.
+Tetris.Piece.prototype.setGravitized = function(value) {
+	// Set the pieces isGravitized to value
+	this.isGravitized = value;
+	// Apply it to all this.blocks
+	this.update();
+};
+// Block[]: Sets isGravitized for all Block objects in this Piece to false. Empties and returns its Blocks.
+Tetris.Piece.prototype.settle = function() {
+	var myBlocks = [];
+
+	// Set all this.currentPiece.blocks.isGravitized to false
+	this.isGravitized = false;
+	this.update();
+
+	// Empty and return them
+	for (block in this.blocks) {
+		myBlocks.push(this.blocks[block]);
+	}
+	this.blocks = [];
+	return myBlocks;
 };
 
 
@@ -224,31 +271,54 @@ Tetris.LogicHandler.prototype.setStepDuration = function(milliseconds) {
 };
 // void: Executes 1 time step of the game.
 Tetris.LogicHandler.prototype.executeStep = function() {
-	// Step 1:
+	var pieceBlocks;
+
 	// Remove filled rows
 	if (this.removeFilledRows()) {
 		this.updateAllGameObjects();
 		this.numOfTimeSteps ++;
 		return;
 	}
-	// OR
-	// Move gravitized blocks down
+	// Or, Move gravitized blocks down
 	else if (this.moveGravitizedBlocks()) {
-		this.updateAllGameObjects();
+		// Check if a ground breach has occured.
+		if (this.checkBoundries() == "ground") {
+			// Move piece up, settle it, and drop a new piece
+			this.currentPiece.moveUp();
+			pieceBlocks = this.currentPiece.settle();
+			for (block in pieceBlocks) {
+				this.blocks.push(pieceBlocks[block]);
+			}
+			this.dropPiece();
+		}
+		// Check if a piece collision has occured.
+		else if (this.checkCollisions()) {
+			// Move piece up, settle it, and drop a new piece
+			this.currentPiece.moveUp();
+			pieceBlocks = this.currentPiece.settle();
+			for (block in pieceBlocks) {
+				this.blocks.push(pieceBlocks[block]);
+			}
+			this.dropPiece();
+		}
+		// Otherwise, re-gravitate currentPiece
+		else {
+			this.currentPiece.setGravitized(true);
+		}
+
+		// Increment the time step counter
 		this.numOfTimeSteps ++;
+
+		// Update sprites
+		this.updateAllGameObjects();
 		return;
 	}
-	// OR
-	// Drop a new piece
+	// Or, Drop a new piece
 	else {
 		this.dropPiece();
 		this.numOfTimeSteps ++;
+		return;
 	}
-
-	// Step 2:
-	// Check block conditions and set gravity on all blocks.
-	// Check conditions on all blocks[], set gravity accordingly.
-	// Check conditions on currentPiece.blocks[], set gravity accordingly.
 };
 // boolean: Checks for completed rows, and flags all blocks in row to be removed. Returns true if a completed row was found.
 Tetris.LogicHandler.prototype.removeFilledRows = function() {
@@ -282,12 +352,14 @@ Tetris.LogicHandler.prototype.moveGravitizedBlocks = function() {
 	for (block in this.blocks) {
 		if (this.blocks[block].isGravitized) {
 			this.blocks[block].moveDown();
+			this.blocks[block].isGravitized = false;
 			moved = true;
 		}
 	}
 	// Move gravitized currentPiece
 	if (this.currentPiece != undefined && this.currentPiece.isGravitized) {
 		this.currentPiece.moveDown();
+		this.currentPiece.setGravitized(false);
 		moved = true;
 	}
 
@@ -313,11 +385,11 @@ Tetris.LogicHandler.prototype.updateAllGameObjects = function() {
 	// Update this.currentPiece
 	if (this.currentPiece != undefined) { this.currentPiece.update(); }
 };
-// Piece: Returns a randomly selected Piece from this.allPossiblePieces.
+// ShapeInfo: Returns ShapeInfo for a randomly selected Piece from this.allPossiblePieces.
 Tetris.LogicHandler.prototype.pickRandomPiece = function() { 
 	return this.allPossiblePieces[Math.floor(Math.random() * this.allPossiblePieces.length)];
 };
-// string: Check if a block has breached the game board boundries. Returns "none" if no boundries breached. Returns "left" if left boundry breached. Returns "right" if right boundry breached. 
+// string: Check if a block has breached the game board boundries. Returns "none" if no boundries breached. Returns "left" if left boundry breached. Returns "right" if right boundry breached. Returns "ground" if ground is breached. Returns "ceiling" if ceiling is breached.
 Tetris.LogicHandler.prototype.checkBoundries = function() {
 	var breached, thisBlock;
 
@@ -327,21 +399,39 @@ Tetris.LogicHandler.prototype.checkBoundries = function() {
 		if (thisBlock.gamePosX < 0) {
 			breached = "left";
 		}
-		if (thisBlock.gamePosX > this.numOfColumns - 1) {
+		else if (thisBlock.gamePosX > this.numOfColumns - 1) {
 			breached = "right";
+		}
+		else if (thisBlock.gamePosY < 0) {
+			breached = "ceiling";
+		}
+		else if (thisBlock.gamePosY > this.numOfRows - 1) {
+			breached = "ground";
 		}
 	}
 
 	return breached;
 };
-Tetris.LogicHandler.prototype.checkBlockConditions = function() { // void: Check ground conditions on all blocks not in currentPiece; set gravity accordingly.
-};
-Tetris.LogicHandler.prototype.checkCurrentPieceConditions = function() { // void: Check ground conditions on all currentPiece blocks; set gravity accordingly.
+// boolean: Returns true if any Blocks in currentPiece are overlapping any Blocks in this.blocks, otherwise returns false.
+Tetris.LogicHandler.prototype.checkCollisions = function() {
+	var currentPieceBlock, stationaryBlock, collided;
+	collided = false;
+
+	for (block1 in this.currentPiece.blocks) {
+		currentPieceBlock = this.currentPiece.blocks[block1];
+		for (block2 in this.blocks) {
+			stationaryBlock = this.blocks[block2];
+			if (currentPieceBlock.gamePosX == stationaryBlock.gamePosX && currentPieceBlock.gamePosY == stationaryBlock.gamePosY) {
+				collided = true;
+			}
+		}
+	}
+
+	return collided;
 };
 
 // Game class: Its functions (init, preload, create, etc.) are called by the Phaser State Manager
 Tetris.Game = function(game) {
-
 };
 // Called by the State Manager before anything else.
 Tetris.Game.prototype.init = function() {
@@ -522,7 +612,8 @@ Tetris.Game.prototype.create = function() {
 
 		switch (e.keyCode) {
 			case Phaser.Keyboard.UP:
-				this.logic.currentPiece.rotate();
+				this.logic.currentPiece.rotateRight();
+				// If the rotation breaches a boundry, move the piece to correct it. In Tetris terms, perform a "wall kick".
 				while (this.logic.checkBoundries() != "none") {
 					if (this.logic.checkBoundries() == "left") {
 						this.logic.currentPiece.moveRight();
@@ -531,11 +622,20 @@ Tetris.Game.prototype.create = function() {
 						this.logic.currentPiece.moveLeft();
 					}
 				}
+				// If the rotation causes a block collision, undo the rotation.
+				if (this.logic.checkCollisions()) {
+					this.logic.currentPiece.rotateLeft();
+				}
 				this.logic.updateAllGameObjects();
 				break;
 			case Phaser.Keyboard.RIGHT:
 				this.logic.currentPiece.moveRight();
+				// If the movement breaches a boundry, undo it.
 				if (this.logic.checkBoundries() == "right") {
+					this.logic.currentPiece.moveLeft();
+				}
+				// If this movement causes a block collision, undo it.
+				if (this.logic.checkCollisions()) {
 					this.logic.currentPiece.moveLeft();
 				}
 				this.logic.updateAllGameObjects();
@@ -545,7 +645,12 @@ Tetris.Game.prototype.create = function() {
 				break;
 			case Phaser.Keyboard.LEFT:
 				this.logic.currentPiece.moveLeft();
+				// If the movement breaches a boundry, undo it.
 				if (this.logic.checkBoundries() == "left") {
+					this.logic.currentPiece.moveRight();
+				}
+				// If this movement causes a block collision, undo it.
+				if (this.logic.checkCollisions()) {
 					this.logic.currentPiece.moveRight();
 				}
 				this.logic.updateAllGameObjects();
@@ -559,351 +664,3 @@ Tetris.Game.prototype.create = function() {
 // Called by World.update ~60hz.
 Tetris.Game.prototype.update = function() {
 };
-
-
-
-/*
-=
-=
-=
-=
-=
-Tetris.Game = function(game) {
-};
-
-Tetris.Game.prototype = {
-
-	init: function() {
-		// Define global variables
-
-		// Number of rows in the game
-		this.numOfRows = 20;
-
-		// Number of blocks in a row
-		this.numOfBlocksInRow = 10;
-
-		// Holds all block objects in the game
-		this.blocks = [];
-
-		// Holds all block objects in the current piece
-		this.currentPiece = [];
-
-		// Holds the type number for the next block
-		this.nextPieceCode = undefined;
-	},
-
-	preload: function() {
-		// Load image assets
-		this.load.image("divider-img", "../assets/divider.png")
-		this.load.image("score-label-img", "../assets/score-label.png")
-		this.load.image("next-label-img", "../assets/next-label.png")
-		this.load.image("red-block-img", "../assets/red-block.png")
-		this.load.image("orange-block-img", "../assets/orange-block.png")
-		this.load.image("yellow-block-img", "../assets/yellow-block.png")
-		this.load.image("green-block-img", "../assets/green-block.png")
-		this.load.image("blue-block-img", "../assets/blue-block.png")
-		this.load.image("violet-block-img", "../assets/violet-block.png")
-	},
-
-	create: function() {
-		// Create the divider sprite
-		var divider = this.game.add.sprite(241.5, 0, "divider-img");
-		divider.scale = new PIXI.Point(1.5, 1.5);
-		divider.anchor = new PIXI.Point(0, 0);
-		this.game.physics.arcade.enable(divider);
-
-		// Create "Next" label
-		var nextLabel = this.game.add.sprite(312, 39, "next-label-img");
-		nextLabel.scale = new PIXI.Point(1.5, 1.5);
-		nextLabel.anchor = new PIXI.Point(0.5, 0.5);
-
-		// Create "Score" label
-		var scoreLabel = this.game.add.sprite(312, 411, "score-label-img");
-		scoreLabel.scale = new PIXI.Point(1.5, 1.5);
-		scoreLabel.anchor = new PIXI.Point(0.5, 0.5);
-
-		// Create a timer to determine the duration between each "step" of the game
-		var stepTimer = this.game.time.create(false) // autoDestroy false
-		stepTimer.loop(500, this.executeStep, this);
-		stepTimer.start();
-	},
-
-	update: function() {
-	},
-
-	executeStep: function() {
-
-		// Check for rows to clear
-		if (this.clearCompletedRows()) {
-			this.updateBlocks();
-			return;
-		}
-		// Or, update blocks with gravity
-		else if (this.applyGravity()) {
-			this.updateBlocks();
-			return;
-		}
-		// Or, introduce new piece
-		else {
-			this.createRandStandardPiece();
-			this.updateBlocks();
-		}
-	},
-
-	updateBlocks: function() {
-		// For all blocks
-		var currentBlock = undefined;
-		for (block in this.blocks) {
-			currentBlock = this.blocks[block]
-
-			// Delete sprite/obj for removed blocks
-			if (currentBlock.removed) {
-				currentBlock.sprite.destroy();
-				this.blocks.splice(block);
-				continue;
-			}
-
-			// Change block sprites pixel pos to reflect game pos
-			currentBlock.sprite.x = this.gamePosToCoord(currentBlock.gamePosX);
-			currentBlock.sprite.y = this.gamePosToCoord(currentBlock.gamePosY);
-		}
-	},
-
-	clearCompletedRows: function() {
-		return false;
-	},
-
-	applyGravity: function() {
-		var didBlockMove = false;
-
-		// Move the blocks with gravity
-		for (block in this.blocks) {
-			if (this.blocks[block].gravity == 1) {
-				this.blocks[block].gamePosY += 1;
-				didBlockMove = true;
-			}
-		}
-
-		// Check if any block in currentPiece has hit a stationary block or the floor
-		// if so, stop the currentPiece
-		for (block in this.currentPiece) {
-			var currentBlock = this.currentPiece[block];
-
-			// Get the object occupying the space below the currentBlock
-			var belowCurrentBlock = undefined;
-			for (block2 in this.blocks) {
-				if (this.blocks[block2].gamePosX == currentBlock.gamePosX && this.blocks[block2].gamePosY == currentBlock.gamePosY + 1) {
-					belowCurrentBlock = this.blocks[block2];
-				}
-			}
-
-			// If the currentBlock is on the bottomRow...
-			if (currentBlock.gamePosY == this.numOfRows - 1) {
-				// Freeze the currentPiece
-				for (block3 in this.currentPiece) {
-					this.currentPiece[block3].gravity = 0;
-				}
-				// Stop the loop
-				break;
-			}
-			// Else, if the belowCurretBlock is a static block...
-			else if (belowCurrentBlock != undefined && belowCurrentBlock.gravity == 0) {
-				// Freeze the currentPiece
-				for (block3 in this.currentPiece) {
-					this.currentPiece[block3].gravity = 0;
-				}
-				// Stop the loop
-				break;
-			}
-		}
-		return didBlockMove;
-	},
-
-	// Using this.nextPiece, puts nextPiece on the board and picks a new random nextPiece.
-	createRandStandardPiece: function() {
-		var currentPieceCode = undefined;
-
-		// If nextPiece exists, make it currentPiece and pick another one.
-		if (this.nextPieceCode != undefined) {
-			currentPieceCode = this.nextPieceCode;
-			this.nextPieceCode = Math.floor(Math.random() * 7);
-		}
-		// Else, pick 2
-		else {
-			currentPieceCode = Math.floor(Math.random() * 7);
-			this.nextPieceCode = Math.floor(Math.random() * 7);
-		}
-
-		// Make the piece
-		switch (currentPieceCode) {
-			// I shape
-			case 0:
-				this.currentPiece = this.createPiece(
-					[ 1, 0, 0, 0,
-					  1, 0, 0, 0,
-					  1, 0, 0, 0,
-					  1, 0, 0, 0 ],
-					4,
-					0,
-					1
-				);
-				break;
-			// J shape
-			case 1:
-				this.currentPiece = this.createPiece(
-					[ 0, 2, 0, 0,
-					  0, 2, 0, 0,
-					  2, 2, 0, 0,
-					  0, 0, 0, 0 ],
-					4,
-					0,
-					1
-				);
-				break;
-			// L shape
-			case 2:
-				this.currentPiece = this.createPiece(
-					[ 3, 0, 0, 0,
-					  3, 0, 0, 0,
-					  3, 3, 0, 0,
-					  0, 0, 0, 0 ],
-					4,
-					0,
-					1
-				);
-				break;
-			// O shape
-			case 3:
-				this.currentPiece = this.createPiece(
-					[ 4, 4, 0, 0,
-					  4, 4, 0, 0,
-					  0, 0, 0, 0,
-					  0, 0, 0, 0 ],
-					4,
-					0,
-					1
-				);
-				break;
-			// Z shape
-			case 4:
-				this.currentPiece = this.createPiece(
-					[ 5, 5, 0, 0,
-					  0, 5, 5, 0,
-					  0, 0, 0, 0,
-					  0, 0, 0, 0 ],
-					4,
-					0,
-					1
-				);
-				break;
-			// T shape
-			case 5:
-				this.currentPiece = this.createPiece(
-					[ 0, 6, 0, 0,
-					  6, 6, 6, 0,
-					  0, 0, 0, 0,
-					  0, 0, 0, 0 ],
-					4,
-					0,
-					1
-				);
-				break;
-			// S shape
-			case 6:
-				this.currentPiece = this.createPiece(
-					[ 0, 4, 4, 0,
-					  4, 4, 0, 0,
-					  0, 0, 0, 0,
-					  0, 0, 0, 0 ],
-					4,
-					0,
-					1
-				);
-				break;
-		}
-	},
-
-	createPiece: function(blockArray, gamePosX, gamePosY, initGravity) {
-		var len, blockPosX, blockPosY, blockColor;
-		var newPiece = [];
-
-		len = blockArray.length;
-		for (i = 0; i < len; i++) {
-			if (blockArray[i] == 0) { continue; }
-			else {
-				// Get the blocks global position
-				blockPosX = gamePosX + (i % 4);
-				blockPosY = gamePosY + Math.floor(i/4);
-
-				// Resolve the blocks color
-				switch(blockArray[i]) {
-					case 1:
-						blockColor = "red-block-img";
-						break;
-					case 2:
-						blockColor = "orange-block-img";
-						break;
-					case 3:
-						blockColor = "yellow-block-img";
-						break;
-					case 4:
-						blockColor = "green-block-img";
-						break;
-					case 5:
-						blockColor = "blue-block-img";
-						break;
-					case 6:
-						blockColor = "violet-block-img";
-						break;
-				}
-			}
-
-			// Create the block and add it to newPiece 
-			newPiece.push(this.createBlock(blockPosX, blockPosY, blockColor, 1));
-		}
-
-		return newPiece
-	},
-
-	// Creates a new block at the given gamePos and adds it to this.blocks
-	// Returns the newly created block.
-	createBlock: function(gamePosX, gamePosY, color, initGravity) {
-		var newBlock = {
-			sprite: this.game.add.sprite(this.gamePosToCoord(gamePosX), this.gamePosToCoord(gamePosY), color),
-			gravity: initGravity,
-			gamePosX: gamePosX,
-			gamePosY: gamePosY,
-			removed: false
-		};
-		newBlock.sprite.scale = new PIXI.Point(1.5, 1.5);
-
-		// Add this block to the global blocks array
-		this.blocks.push(newBlock);
-
-		return newBlock;
-	},
-
-	// Converts the given game position into a pos index in this.blocks
-	gamePosToPosIndex: function(posX, posY) {
-		return ((posY * 10) + posX);
-	},
-
-	// Converts the given pos index from this.blocks into a game position
-	posIndexToGamePos: function(posIndex) {
-		var blockPosX = posIndex % 10;
-		var blockPosY = Math.floor(posIndex / 10);
-		return [blockPosX, blockPosY];
-	},
-
-	// Converts the given gamePosition into a pixel coordinate.
-	gamePosToCoord: function(pos) {
-		return ((pos * 1.5 * 16) + 1);
-	},
-
-	// Converts the given pixel coordinate into a gamePosition.
-	coordToGamePos: function(coord) {
-		return ((coord - 1) / (1.5 * 16));
-	}
-
-};
-*/
